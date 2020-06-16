d0438e5b9b6ae96b3911a359b390e35e72803646
lucas <lucas.tiziani@gatech.edu>
2020-06-16 18:15:01 -0400

    verified pca9685_driver functionality

--- package/pwm.py
+++ package/pwm.py
@@ -1,56 +1,73 @@
+from pca9685_driver import Device
+import time
 
-
-
-# sudo apt-get install python3-smbus
-
-
-import smbus
-
-
-i2c_channel = 1 #TODO
-
-i2c_address = 0x00 #TODO
-
-# register addresses
-
-
-class Pwm:
-	def __init__(self, i2c_channel, i2c_address):
-
-
-		self.i2c_address
-		self.bus = smbus.SMBus(i2c_channel) # initialize I2C
-
-
-
-
-	def updateAll(self, dutys):
-		# Update all duty cycles
-
-		#TODO: loop over pressure channels:
-
-		self.bus.write_i2c_block_data(self.i2c_address, reg, val)
-
-
-
-
-#DEBUG: 
-if __name__=="main":
-	import time
-	import numpy as np
-
-	pwm = Pwm(i2c_channel, i2c_address)
-
-	dutys = np.array([0,0,0,0])
+if __name__=="__main__":
+	dev = Device(0x40)
+	dev.set_pwm_frequency(25) # (24- Hz)
+	dev.set_pwm(0, 2047) # (0-4095 counts)
+	
 	while True:
-		dutys = dutys + 10
-		for i, duty in enumerate(dutys):
-	     	if duty > 100:
-	            dutys[i] = 0
-
-
-		pwm.updateAll(dutys)
+		# ~ dev.set_pwm(0, 0)
+		# ~ time.sleep(1)
+		# ~ dev.set_pwm(0, 2047)
+		# ~ time.sleep(1)
+		# ~ dev.set_pwm(0, 4096)
 		time.sleep(1)
+
+
+
+
+
+# ~ # sudo apt-get install python3-smbus
+
+
+# ~ import smbus
+
+
+# ~ i2c_channel = 1 #TODO
+
+# ~ i2c_address = 0x00 #TODO
+
+# ~ # register addresses
+
+
+# ~ class Pwm:
+	# ~ def __init__(self, i2c_channel, i2c_address):
+
+
+		# ~ self.i2c_address
+		# ~ self.bus = smbus.SMBus(i2c_channel) # initialize I2C
+
+
+
+
+	# ~ def updateAll(self, dutys):
+		# ~ # Update all duty cycles
+
+		# ~ #TODO: loop over pressure channels:
+
+		# ~ self.bus.write_i2c_block_data(self.i2c_address, reg, val)
+
+
+
+
+# ~ #DEBUG: 
+# ~ if __name__=="main":
+	# ~ import time
+	# ~ import numpy as np
+
+	# ~ pwm = Pwm(i2c_channel, i2c_address)
+
+	# ~ dutys = np.array([0,0,0,0])
+	# ~ while True:
+		# ~ dutys = dutys + 10
+		# ~ for i, duty in enumerate(dutys):
+	     	# ~ if duty > 100:
+	            # ~ dutys[i] = 0
+
+
+		# ~ pwm.updateAll(dutys)
+		# ~ time.sleep(1)
 
 
 
