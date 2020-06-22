caafbfbefa745b4c59e85ba398fc9e631d68f4ea
lucas <lucas.tiziani@gatech.edu>
2020-06-22 19:12:36 -0400

    removed chamber_info usage

--- package/plotter.py
+++ package/plotter.py
@@ -20,15 +20,13 @@ class PlotWindow(QMainWindow, plotwindow.Ui_MainWindow):
 
 	signal_plotter_closed = QtCore.pyqtSignal()
 
-	def __init__(self, chamber_info, chambers, buffer_len, signal_stop_plot):
+	def __init__(self, chambers, buffer_len, signal_stop_plot):
 		super(self.__class__, self).__init__()
 		self.setupUi(self)
 
-		self.chamber_info = chamber_info
 		self.chambers = chambers
 		self.buffer_len = buffer_len
 		self.signal_stop_plot = signal_stop_plot
-
 
 		colors = (
 			(0, 114, 189),
@@ -41,7 +39,7 @@ class PlotWindow(QMainWindow, plotwindow.Ui_MainWindow):
 		buffer = [0]*self.buffer_len
 		idx = 0
 		self.widget_chamberPlot.addLegend()
-		for name in self.chamber_info:
+		for name in self.chambers:
 			pen = pg.mkPen(color=colors[idx], width=2)
 			self.lines[name] = self.widget_chamberPlot.plot(buffer, buffer, pen=pen, name=name)
 			idx = idx + 1
@@ -57,7 +55,7 @@ class PlotWindow(QMainWindow, plotwindow.Ui_MainWindow):
 			y = self.lines[name].yData 
 
 			x = np.append(x[1:], t)
-			y = np.append(y[1:], chamber.pres_meas)
+			y = np.append(y[1:], chamber['obj'].pres_meas)
 
 			self.lines[name].setData(x,y)
 
