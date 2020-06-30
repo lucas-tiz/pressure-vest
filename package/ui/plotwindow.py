5b23236ca7197625b3050e850e110a5db170b9b1
Lucas <lucas.tiziani@gatech.edu>
2020-06-30 10:48:32 -0400

    switched plot window to qwidget

--- package/ui/plotwindow.py
+++ package/ui/plotwindow.py
@@ -8,29 +8,19 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.widget_chamberPlot = PlotWidget(self.centralwidget)
-        self.widget_chamberPlot.setGeometry(QtCore.QRect(80, 70, 661, 231))
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(749, 314)
+        self.widget_chamberPlot = PlotWidget(Form)
+        self.widget_chamberPlot.setGeometry(QtCore.QRect(40, 40, 661, 231))
         self.widget_chamberPlot.setObjectName("widget_chamberPlot")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
 
-    def retranslateUi(self, MainWindow):
+    def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        Form.setWindowTitle(_translate("Form", "Form"))
 
 from pyqtgraph import PlotWidget
