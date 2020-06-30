1096ee849d67424302e4f20d44b3a0eb32825069
Lucas <lucas.tiziani@gatech.edu>
2020-06-30 10:58:34 -0400

    added control algorithm

--- package/chamber.py
+++ package/chamber.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+from numpy import sign
 
 class PressureChamber:
 	# Manage interaction between GUI signals & pressure control
@@ -21,18 +21,22 @@ class PressureChamber:
 		self.groupbox.toggled.connect(self.enableChamber) # checkbox update
 
 		# chamber attributes
+		self.enabled = True
 		self.pres_meas = 0 # initialize pressure measurement
 		self.pres_set = 0 # initialize pressure setpoint
-		self.enabled = True
-		self.duty = 0 #DEBUG
+		self.err_int = 0 # initialize integral error
+		self.err_prev = 0 # initialize previous error
+		self.duty = {'inflation':0, 'deflation':0} #DEBUG
 
 
 	def enableChamber(self):
 		# Enable or disable pressure chamber
 		if self.groupbox.isChecked() == 1:
 			self.enabled = True # enable controller
+			self.pres_set = self.spinbox.value() # setpoint to GUI value
 		else:
 			self.enabled = False # disable controller
+			self.pres_set = 0 # setpoint to zero
 
 
 	def updateSetpoint(self, n):
@@ -64,15 +68,50 @@ class PressureChamber:
 		self.pres_meas = pressure
 
 
-	def calcControl(self):
-		if self.enabled:
-			self.duty = self.duty + 1 #DEBUG
-			duty = self.duty #DEBUG
-			#TODO: control law
-		else:
-			duty = 0
+	def calcControl(self, control):
+		err = self.pres_meas - self.pres_set # (psi) pressure error
 
-		return duty
+		if self.pres_set == 0: # if setpoint is zero
+			if err > control['noise_threshold']: # and not within sensor noise range
+				self.duty['inflation'] = 0 # fully close inflation valve
+				self.duty['deflation'] = 100 # fully open vent valve
+			else: # if within sensor noise range
+				self.duty['inflation'] = 0 # fully close inflation valve
+				self.duty['deflation'] = 0 # fully close vent valve
+			self.err_int = 0 # reset integral
+
+		elif (abs(err) <= control['differential_gap']): # if nonzero setpoint, but within differential gap
+			self.duty['inflation'] = 0 # fully close inflation valve
+			self.duty['deflation'] = 0 # fully close vent valve
+			self.err_int = 0 # reset integral
+
+		else: # if nonzero setpoint, out of differential gap
+			if err < 0: # if below setpoint
+				mode = 'inflation'
+				self.duty['deflation'] = 0 # close deflation valve
+			else: # if above setpoint
+				mode = 'deflation'
+				self.duty['inflation'] = 0 # close inflation valve
+			kp = control[mode]['kp']
+			ki = control[mode]['ki']
+			kd = control[mode]['kd']
+
+			# calculate control variables
+			self.err_int = self.err_int + err
+			err_der = err - self.err_prev
+			self.err_prev = err 
+
+			# reset integral windup
+			if (self.err_int*ki > control['windup_limit']):
+				self.err_int = control['windup_limit']/ki
+			elif (self.err_int*ki < -control['windup_limit']):
+				self.err_int = -control['windup_limit']/ki
+
+			# calculate control & impose saturation limits
+			u = control['deadzone'] + (kp*err + ki*self.err_int + kd*err_der)*sign(err)
+			self.duty[mode] = min(u, 100)
+
+		return self.duty
 
 
 	def updateMeasurementDisplay(self):
