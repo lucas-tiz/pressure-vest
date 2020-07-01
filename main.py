e7689c47821632fd04ecd835dcdaa5d6c3b6d620
Lucas <lucas.tiziani@gatech.edu>
2020-07-01 13:06:51 -0400

    switched chamber config to yaml file

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
-import json
+
 
 if __name__ == "__main__":
 	import sys
+	import yaml
 	from PyQt5.QtWidgets import QApplication
 	from package import vestapp
 
-	# chamber_info = {'aur':{'adc':0, 'pwm':0}, #TODO: put this in main
-	# 				'aul':{'adc':1, 'pwm':1},
-	# 				'alr':{'adc':2, 'pwm':2},
-	# 				'all':{'adc':3, 'pwm':3} }
-
-	with open('chamber_config.txt') as f: 
-		chamber_info = json.load(f)
-	
+	with open('chamber_config.yaml') as f: 
+		chamber_config = yaml.load(f, Loader=yaml.FullLoader)
 
 	app = QApplication(sys.argv)
 	app.setStyle('Fusion')
@@ -21,21 +16,14 @@ if __name__ == "__main__":
 	plot = False
 	debug_gui = False
 	tune = False
-	if "plot" in sys.argv[1:]:
+	if "plot" in sys.argv[1:]: # option for plotting pressures
 		plot = True
-	if "debug_gui" in sys.argv[1:]:
+	if "debug_gui" in sys.argv[1:]: # option for debugging GUI (no GPIO/peripherals)
 		debug_gui = True
-	if "tune" in sys.argv[1:]:
+	if "tune" in sys.argv[1:]: # option for tuning system/control parameters
 		tune = True
 
-	form = vestapp.VestController(chamber_info, plot=plot, debug_gui=debug_gui, tune=tune)
+	form = vestapp.VestController(chamber_config, plot=plot, debug_gui=debug_gui, tune=tune)
 	form.show()
-
-
-	# # plotter
-	# from package import plotter
-	# plot_window = plotter.PlotWindow()
-	# plot_window.show()
-
 	sys.exit(app.exec_())
 
